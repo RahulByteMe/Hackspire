67b65db1fd2708f908ac3c935314f400a8328818
Bhavdeepsingh7 <bhavdeepmakkar@gmail.com>
2026-02-07 22:53:08 +0530

    vote controller and route

---
+++ backend/src/routes/vote.routes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { castVote } from "../controllers/vote.controller.js";
+import { voteValidator } from "../validators/election.validator.js";
+import { validate } from "../middlewares/validator.middleware.js";
+
+const router = express.Router();
+
+router.post("/cast-vote",voteValidator(),validate,castVote);
+
+export default router;
